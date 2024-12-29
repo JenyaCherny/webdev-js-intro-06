481eb2630a5746262507d837e350fdd799cf7cd3
JenyaCherny <jenya.cherny@gmail.com>
2024-12-29 17:43:06 -0500

    compeleted all functions in assignmentjs

--- js/assignment.js
+++ js/assignment.js
@@ -5,10 +5,15 @@ const evenOrOddElement = document.getElementById("even-or-odd");
 const sumTheNumbersElement = document.getElementById("sum-the-numbers");
 const createNumberArrayElement = document.getElementById("create-number-array");
 
-function evenOrOdd() {
+function evenOrOdd() { 
     const num = 3;
-    // Write the logic to decide if the variable "num" is even or odd
-    // and set the element's value the string "Even" or "Odd" accordingly
+    const resultElement = document.getElementById("even-or-odd"); 
+    if (num % 2 === 0) { 
+        resultElement.textContent = 'Even'; 
+            } 
+            else 
+            { resultElement.textContent = 'Odd';
+            }
 
 }
 
@@ -17,6 +22,9 @@ function sumTheNumbers() {
     // Write the logic to sum the numbers 1 through 10
     // using a for loop. Check the expected output
     // on the assignment page
+    for (let i = 1; i <= 10; i++) 
+        { sum += i; } 
+    console.log(sum);
 
 }
 
@@ -26,12 +34,17 @@ function createNumberArray() {
     // Write the logic that loops 10 times and adds the value
     // to numberArray each iteration. Check the expected output
     // on the assignment page
+    for (let i = 1; i <= 10; i++) 
+        { numberArray.push(i); } 
+    console.log(numberArray);
 
 }
 
 function render() {
     // Call the created functions
-
+    evenOrOdd()
+    sumTheNumbers()
+    createNumberArray()
 }
 
 submissionBtn.addEventListener("click", function () {
