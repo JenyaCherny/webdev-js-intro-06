a37519c3c0ae8c148c0a8d78bac895adacfaf8a8
JenyaCherny <jenya.cherny@gmail.com>
2024-12-29 16:51:24 -0500

    did experiments in index.js and commented them out

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+for (let n = 0; 
+    n < 8; n++) {
+    console.log(n);
+}
+
+
+let i = 12;
+do {
+    console.log(i);
+    i++;
+} while (i < 22);
+
+let b = 0;
+while (b < 10) {
+    console.log(b);
+    b++;
+} 
